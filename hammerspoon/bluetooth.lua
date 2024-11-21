14e53822708f4c33237d5a156e793d2731e55e3f
Rohith Pudari <27728974+rohithpudari@users.noreply.github.com>
2024-11-21 16:03:22 -0500

    disconnect living room speaker when mac sleeps

---
+++ hammerspoon/bluetooth.lua
@@ -0,0 +1,25 @@
+-- script adapted from https://gist.github.com/ysimonson/fea48ee8a68ed2cbac12473e87134f58 
+
+require "string"
+
+function checkBluetoothResult(rc, stderr, stderr)
+    if rc ~= 0 then
+        print(string.format("Unexpected result executing `blueutil`: rc=%d stderr=%s stdout=%s", rc, stderr, stdout))
+    end
+end
+
+function bluetooth(power)
+    local living_speaker = "7c-d9-5c-84-16-d7"
+    -- /usr/local/bin for Intel Macs, and /opt/homebrew/bin for M1 Macs
+    local t = hs.task.new("/usr/local/bin/blueutil", checkBluetoothResult, {"--disconnect", living_speaker})
+    t:start()
+end
+
+function f(event)
+  if event == hs.caffeinate.watcher.systemWillSleep then
+    bluetooth()
+  end
+end
+
+watcher = hs.caffeinate.watcher.new(bluetooth)
+watcher:start()
