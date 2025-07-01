05c09e7d0a40e2e524539ef4325622c78afbccf2
Rohith Pudari <27728974+rohithpudari@users.noreply.github.com>
2025-07-01 16:12:00 -0400

    gpu server

--- hammerspoon/config.lua
+++
@@ -1,9 +0,0 @@
-MACBOOK_MONITOR = "Built-in Retina Display"
-
-RIGHT_MONITOR = "KB272HL"
-MAIN_MONITOR = "DELL SE2722H"
-
-HOME_MONITOR = "HP Z24n G3"
-HOME_MONITOR2 = "BenQ GW2785TC"
-
-IDEs = { "Code", "Alacritty" }
