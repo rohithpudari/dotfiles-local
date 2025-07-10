9691bd3318a611299271aeaccaf3002de0206a14
Rohith Pudari <27728974+rohithpudari@users.noreply.github.com>
2025-07-10 11:03:04 -0400

    nvim and zsh autosuggest

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,4 +1,4 @@
-return {}
+-- old version - that causes error as blink is updated to new API call and lazyvim is still not updated, remove new version and use old when lazyvim is updated
 -- return {
 -- 	-- change blink config
 -- 	"saghen/blink.cmp",
@@ -6,3 +6,17 @@ return {}
 -- 		keymap = { preset = "super-tab" },
 -- 	},
 -- }
+--
+return {
+	"saghen/blink.cmp",
+	opts = {
+		keymap = {
+			preset = "super-tab",
+			["<Tab>"] = {
+				require("blink.cmp.keymap.presets").get("super-tab")["<Tab>"][1],
+				require("lazyvim.util.cmp").map({ "snippet_forward", "ai_accept" }),
+				"fallback",
+			},
+		},
+	},
+}
